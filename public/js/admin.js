b69b704ff38b3ef53018ea9798ba1c9f98a99053
liuzuan <liuzuann@gmail.com>
2021-09-24 17:31:20 +0800

    chg: 增删功能

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,11 +1,37 @@
-const { reactive, setup, createApp, getCurrentInstance } = Vue;
+const { reactive, setup, createApp, getCurrentInstance, watch, nextTick } = Vue;
 
 const zh_cn = data['zh-cn'];
 const en_us = data['en-us'];
 
-
 let cachedTableData; // 切换未翻译时缓存数据
 const cacheZhSet = new Set(); //缓存中文去重
+
+class TableItem {
+  constructor(prop) {
+    const { pid = '', id = creatId(), key, en, zh, children } = prop;
+    this.id = id;
+    this.pid = pid;
+    this.key = key;
+    if (en) this.en = en;
+    if (zh) this.zh = zh;
+    if (children) this.children = children;
+  }
+
+  delSelf(tableData) {
+    if (this.pid) {
+      const parent = tableData.find(({ id }) => id === this.pid);
+      const index = parent.children.findIndex(row => row.id === this.id);
+      parent.children.splice(index, 1);
+    } else {
+      const index = tableData.findIndex(row => row.id === this.id);
+      tableData.splice(index, 1);
+    }
+  }
+
+  addChild(child) {
+    this.children.push(child);
+  }
+}
 
 const app = createApp({
   setup() {
@@ -16,12 +42,18 @@ const app = createApp({
     const [zhUniqueKeys, enUniqueKeys] = diffObjs(zh_cn, en_us);
     const tip1 = zhUniqueKeys.length && `中文keys: ${zhUniqueKeys.join('、')} 在英文中不存在`;
     const tip2 = enUniqueKeys.length && `英文keys: ${enUniqueKeys.join('、')} 在中文中不存在`;
-    
+
     const state = reactive({
       id,
       apps,
       tableData,
       empty: false,
+      showAddForm: false,
+      formData: {
+        key: '',
+        zh: '',
+        en: '',
+      },
       tip1,
       tip2,
     });
@@ -33,19 +65,90 @@ const app = createApp({
         })
         .catch(window.alert);
     }
-    function selectAPP(e) {
-      location.pathname = `/fe-i18n/admin/${e.target.value}`;
-    }
+
     function toggleEmpty() {
       state.empty = !state.empty;
       if (state.empty) cachedTableData = state.tableData;
       state.tableData = state.empty ? filterEmpty(state.tableData) : cachedTableData;
     }
+
+    function onDelete(row) {
+      // 删靠前的，页面更新巨慢
+      console.group('del');
+      console.time('data');
+      console.time('page');
+
+      row.delSelf(state.tableData);
+
+      console.timeEnd('data');
+      nextTick(() => {
+        console.timeEnd('page');
+        console.groupEnd();
+      });
+    }
+
+    function onAdd() {
+      state.showAddForm = true;
+      state.formData.pid = '';
+    }
+
+    function onAddChild(row) {
+      state.showAddForm = true;
+      state.formData.pid = row.id;
+    }
+
+    function saveForm() {
+      internalInstance.refs['addForm'].validate(valid => {
+        if (!valid) return;
+        const item = new TableItem(state.formData);
+        const { pid } = item;
+        if (pid) {
+          const parent = state.tableData.find(({ id }) => id === pid);
+          parent.addChild(item);
+        } else {
+          state.tableData.push(item);
+          pageScroll();
+        }
+        resetForm();
+      });
+    }
+    function resetForm() {
+      state.showAddForm = false;
+      internalInstance.refs['addForm'].resetFields();
+    }
+
+    function onAddParent() {
+      globalProperties
+        .$prompt('请填写key', '添加一个外层', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputValidator: val => !!val,
+          inputErrorMessage: '请填写key',
+        })
+        .then(({ value }) => {
+          state.tableData.push(new TableItem({ key: value, children: [] }));
+          pageScroll();
+        })
+        .catch(() => {});
+    }
+
+    watch(
+      () => state.id,
+      () => {
+        location.pathname = `/fe-i18n/admin/${state.id}`;
+      },
+    );
+
     return {
       state,
       submit,
-      selectAPP,
       toggleEmpty,
+      onDelete,
+      onAdd,
+      onAddChild,
+      saveForm,
+      resetForm,
+      onAddParent,
     };
   },
 });
@@ -78,35 +181,25 @@ function filterEmpty(arr) {
     .filter(v => v);
 }
 
-function formatTableData(zh_cn, en_us) {
+function formatTableData(zh_cn, en_us, pid = '') {
   if (typeof zh_cn !== 'object') return;
-
+  // pid: parent id
   return Object.entries(zh_cn)
     .map(([k, v]) => {
       if (typeof v === 'object') {
-        const children = formatTableData(v, en_us[k]);
-        if (children.length) {
-          return {
-            key: k,
-            id: idFunc(),
-            children,
-          };
-        }
+        const id = creatId();
+        const children = formatTableData(v, en_us[k], id);
+        return new TableItem({ pid, key: k, id, children });
       } else if (!cacheZhSet.has(v)) {
         cacheZhSet.add(v);
-        return {
-          key: k,
-          id: idFunc(),
-          zh: v,
-          en: en_us[k],
-        };
+        return new TableItem({ pid, key: k, zh: v, en: en_us[k] });
       }
     })
     .filter(item => !!item);
+}
 
-  function idFunc() {
-    return Math.random().toString(32).slice(-8);
-  }
+function creatId() {
+  return Math.random().toString(32).slice(-8);
 }
 
 function getUpdateData(arr) {
@@ -161,4 +254,10 @@ function diffObjs(s1, s2) {
 
     return result;
   }
+}
+
+function pageScroll(top = document.body.clientHeight) {
+  Vue.nextTick(() => {
+    window.scroll({ top });
+  });
 }
